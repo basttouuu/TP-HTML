c4194b36284ef8ac120efd0c1738d966dbe99990
Hugo PAULIER <hugo.paulier@ynov.com>
2025-10-14 23:09:38 +0200

    V1

--- pagecommu/code.js
+++ pagecommu/code.js
@@ -1,33 +1,33 @@
-
 const formCommentaire = document.getElementById('formulaire-commentaire');
 const commentairesDiv = document.getElementById('commentaires');
 
- 
+
 window.addEventListener('load', () => {
-    const saved = JSON.parse(localStorage.getItem('chillCommentaires') || '[]');
-    saved.forEach(c => ajouterCommentaire(c.nom, c.message));
+  const saved = JSON.parse(localStorage.getItem('chillCommentaires') || '[]');
+  saved.forEach(c => ajouterCommentaire(c.nom, c.message));
 });
 
+
 formCommentaire.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const nom = document.getElementById('nom').value.trim();
-    const message = document.getElementById('message').value.trim();
-    if (!nom || !message) return;
+  e.preventDefault();
+  const nom = document.getElementById('nom').value.trim();
+  const message = document.getElementById('message').value.trim();
+  if (!nom || !message) return;
 
-    ajouterCommentaire(nom, message);
+  ajouterCommentaire(nom, message);
 
 
-    const saved = JSON.parse(localStorage.getItem('chillCommentaires') || '[]');
-    saved.push({ nom, message });
-    localStorage.setItem('chillCommentaires', JSON.stringify(saved));
+  const saved = JSON.parse(localStorage.getItem('chillCommentaires') || '[]');
+  saved.push({ nom, message });
+  localStorage.setItem('chillCommentaires', JSON.stringify(saved));
 
-    formCommentaire.reset();
- });
+  formCommentaire.reset();
+});
+
 
 function ajouterCommentaire(nom, message) {
-    const div = document.createElement('div');
-    div.classList.add('commentaire');
-    div.innerHTML = `<strong>${nom}</strong> : ${message}`;
-    commentairesDiv.appendChild(div);
+  const div = document.createElement('div');
+  div.classList.add('commentaire');
+  div.innerHTML = `<strong>${nom}</strong> : ${message}`;
+  commentairesDiv.appendChild(div);
 }
-
